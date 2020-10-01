0410f732e3cdb113f95346071e5b41e0c5277cd2
Claiton <cdpaulus@inf.ufsm.br>
2020-10-01 09:50:41 -0300

    Inputs correction

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import api from '../../services/api';
 import { FiLogIn } from 'react-icons/fi';
@@ -6,44 +6,44 @@ import { FiLogIn } from 'react-icons/fi';
 import './styles.css';
 import logoImg from '../../assets/logo.png';
 
-export default function Login(){
+export default function Login() {
     const [id, setId] = useState('');
     const history = useHistory();
 
 
-    async function handleLogin(e){
+    async function handleLogin(e) {
         e.preventDefault();
 
-        try{
-           const response = await api.post('sessions', {id});
+        try {
+            const response = await api.post('sessions', { id });
 
-           localStorage.setItem('professorId', id);
-           localStorage.setItem('professorName', response.data.name);
+            localStorage.setItem('professorId', id);
+            localStorage.setItem('professorName', response.data.name);
 
-           history.push('/profile');
-        }catch (error) {
+            history.push('/profile');
+        } catch (error) {
             alert("Erro, tente novamente.");
         }
     }
 
-    return(
+    return (
         <div className="Login-container">
-            
             <section className="form">
                 <div className="img">
-                    <img src={logoImg} alt="Logo"/>
+                    <img src={logoImg} alt="Logo" />
                 </div>
-                
-                <form onSubmit={handleLogin}> 
+
+                <form onSubmit={handleLogin}>
                     <h1>Faça seu Login</h1>
-                    <input 
+                    <input
                         placeholder="Seu ID"
                         value={id}
                         onChange={e => setId(e.target.value)}
+                        required
                     />
                     <button className="button" type="submit">Entrar</button>
                     <Link className="back-link" to="/register">
-                        <FiLogIn size={16} color="#e02041"/>
+                        <FiLogIn size={16} color="#e02041" />
                         Não tenho cadastro
                     </Link>
                 </form>
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,16 +1,16 @@
-import React, {useState} from 'react';
-import {Link, useHistory} from 'react-router-dom';
-import{ FiArrowLeft} from 'react-icons/fi';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
 import './styles.css';
 import api from '../../services/api';
 
-export default function Register(){
+export default function Register() {
     const history = useHistory();
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [siape, setSiape] = useState('');
 
-    async function handleRegister(e){
+    async function handleRegister(e) {
         e.preventDefault();
 
         const data = {
@@ -18,45 +18,46 @@ export default function Register(){
             email,
             siape
         }
-
-        try{
+        try {
             const response = await api.post('professors', data);
             alert(`Seu ID de acesso: ${response.data.id}`);
             history.push('/');
-        }catch (err){
+        } catch (err) {
             alert('Erro, tente novamente.');
         }
     }
 
-    return(
+    return (
         <div className="register-container">
             <div className="content">
                 <section>
                     <h1>Cadastro</h1>
                     <p>Faça seu cadastro...</p>
-
                     <Link className="back-link" to="/">
-                        <FiArrowLeft size={16} color="#e02041"/>
+                        <FiArrowLeft size={16} color="#e02041" />
                         Já tenho Cadastro
                     </Link>
                 </section>
 
                 <form onSubmit={handleRegister}>
-                    <input 
+                    <input
                         placeholder="Nome professor(a)"
                         value={name}
                         onChange={e => setName(e.target.value)}
+                        required
                     />
-                    <input 
-                        type="email" 
+                    <input
+                        type="email"
                         placeholder="Email"
                         value={email}
-                        onChange={e => setEmail(e.target.value)}    
+                        onChange={e => setEmail(e.target.value)}
+                        required
                     />
-                    <input 
+                    <input
                         placeholder="Siape"
                         value={siape}
-                        onChange={e => setSiape(e.target.value)}    
+                        onChange={e => setSiape(e.target.value)}
+                        required
                     />
 
                     <button className="button" type="submit">Cadastrar</button>
