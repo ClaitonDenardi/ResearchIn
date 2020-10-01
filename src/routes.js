36f060745a9adba94b4238db7acebb256bb30cf5
Claiton <cdpaulus@inf.ufsm.br>
2020-10-01 09:18:08 -0300

    Private routes implementation

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { PrivateRoute } from './pages/Login/PrivateRoute';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
 import NewProject from './pages/NewProject';
 
-export default function Routes(){
-    return(
+export default function Routes() {
+    return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={Login}/>
-                <Route path="/register" component={Register}/>
-                <Route path="/profile" component={Profile}/>
-                <Route paht="/projects/new" component={NewProject}/>
+                <Route path="/" exact component={Login} />
+                <Route path="/register" component={Register} />
+                <PrivateRoute path="/profile" component={Profile} />
+                <PrivateRoute paht="/projects/new" component={NewProject} />
             </Switch>
         </BrowserRouter>
     );
