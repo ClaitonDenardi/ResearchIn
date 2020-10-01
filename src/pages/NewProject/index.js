a755de21fc626eea89dc385af6d7fc2809b59491
Claiton <cdpaulus@inf.ufsm.br>
2020-10-01 09:55:35 -0300

    Required fields updated

--- src/pages/NewProject/index.js
+++ src/pages/NewProject/index.js
@@ -1,62 +1,64 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 import { Link, useHistory } from 'react-router-dom'
-import {FiArrowLeft} from 'react-icons/fi'
+import { FiArrowLeft } from 'react-icons/fi'
 import api from '../../services/api'
 
-export default function NewProject(){
+export default function NewProject() {
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
     const [holder, setHolder] = useState('');
     const professorId = localStorage.getItem('professorId');
     const history = useHistory('');
 
-    async function handleNewProject(e){
+    async function handleNewProject(e) {
         e.preventDefault();
 
-        const data ={
-            title, 
-            description, 
+        const data = {
+            title,
+            description,
             holder
         };
 
-        try{
+        try {
             await api.post('projects', data, {
-                headers:{
+                headers: {
                     Authorization: professorId,
                 }
             });
             history.push('/profile');
-        }catch(err){
+        } catch (err) {
             alert("Erro ao cadastrar, tente novamente")
         }
     }
 
 
-    return(
+    return (
         <div className="new-incident-container">
             <div className="content">
                 <section>
                     <h1>Cadastrar novo projeto</h1>
                     <p>Descreva o projeto ...</p>
                     <Link className="back-link" to="/profile">
-                        <FiArrowLeft size={16} color="#e02041"/>
+                        <FiArrowLeft size={16} color="#e02041" />
                         Voltar
                     </Link>
                 </section>
 
                 <form onSubmit={handleNewProject}>
-                    <input 
+                    <input
                         placeholder="Título do Projeto"
                         value={title}
                         onChange={e => setTitle(e.target.value)}
+                        required
                     />
-                    <textarea 
+                    <textarea
                         placeholder="Descrição"
                         value={description}
                         onChange={e => setDescription(e.target.value)}
+                        required
                     />
-                    <input 
+                    <input
                         placeholder="Bolsa"
                         value={holder}
                         onChange={e => setHolder(e.target.value)}
