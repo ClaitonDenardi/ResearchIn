9af03cacabbb4cbdb7028ec01926230d5b38dc8e
Claiton <cdpaulus@inf.ufsm.br>
2020-12-06 20:29:05 -0300

    Ajustes gerais

--- src/pages/NewProject/index.js
+++ src/pages/NewProject/index.js
@@ -1,72 +1,76 @@
-import React, { useState } from 'react';
-import './styles.css';
-import { Link, useHistory } from 'react-router-dom'
-import { FiArrowLeft } from 'react-icons/fi'
-import api from '../../services/api'
+import React, { useState } from "react";
+import "./styles.css";
+import { Link, useHistory } from "react-router-dom";
+import { FiArrowLeft } from "react-icons/fi";
+import api from "../../services/api";
 
 export default function NewProject() {
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
-    const [holder, setHolder] = useState('');
-    const professorId = localStorage.getItem('professorId');
-    const history = useHistory('');
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [holder, setHolder] = useState("");
+  const professorId = localStorage.getItem("professorId");
+  const history = useHistory("");
 
-    async function handleNewProject(e) {
-        e.preventDefault();
+  async function handleNewProject(e) {
+    e.preventDefault();
 
-        const data = {
-            title,
-            description,
-            holder
-        };
+    const data = {
+      title,
+      description,
+      holder,
+    };
 
-        try {
-            await api.post('projects', data, {
-                headers: {
-                    Authorization: professorId,
-                }
-            });
-            history.push('/profile');
-        } catch (err) {
-            alert("Erro ao cadastrar, tente novamente")
-        }
+    try {
+      await api.post("projects", data, {
+        headers: {
+          Authorization: professorId,
+        },
+      });
+      history.push("/profile");
+    } catch (err) {
+      alert("Erro ao cadastrar, tente novamente");
     }
+  }
 
+  return (
+    <div className="new-incident-container">
+      <div className="content">
+        <section style={{ paddingRight: "2%" }}>
+          <h1>Cadastrar novo projeto</h1>
+          <p>
+            Utilize a descrição da melhor maneira possível, seja listando links,
+            materiais, conteúdos, registro do projeto no GAP ou requisitos aos
+            alunos.
+          </p>
+          <Link className="back-link" to="/profile">
+            <FiArrowLeft size={16} color="#e02041" />
+            Voltar
+          </Link>
+        </section>
+        <form onSubmit={handleNewProject}>
+          <input
+            placeholder="Título do Projeto"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+          <textarea
+            placeholder="Descrição"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            required
+          />
+          <input
+            placeholder="Bolsa"
+            value={holder}
+            onChange={(e) => setHolder(e.target.value)}
+          />
 
-    return (
-        <div className="new-incident-container">
-            <div className="content">
-                <section>
-                    <h1>Cadastrar novo projeto</h1>
-                    <p>Descreva o projeto ...</p>
-                    <Link className="back-link" to="/profile">
-                        <FiArrowLeft size={16} color="#e02041" />
-                        Voltar
-                    </Link>
-                </section>
-
-                <form onSubmit={handleNewProject}>
-                    <input
-                        placeholder="Título do Projeto"
-                        value={title}
-                        onChange={e => setTitle(e.target.value)}
-                        required
-                    />
-                    <textarea
-                        placeholder="Descrição"
-                        value={description}
-                        onChange={e => setDescription(e.target.value)}
-                        required
-                    />
-                    <input
-                        placeholder="Bolsa"
-                        value={holder}
-                        onChange={e => setHolder(e.target.value)}
-                    />
-
-                    <button className="button" type="submit">Cadastrar</button>
-                </form>
-            </div>
-        </div>
-    );
+          <button className="button" type="submit">
+            Cadastrar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,68 +1,78 @@
-import React, { useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import { FiArrowLeft } from 'react-icons/fi';
-import './styles.css';
-import api from '../../services/api';
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { FiArrowLeft } from "react-icons/fi";
+import "./styles.css";
+import api from "../../services/api";
 
 export default function Register() {
-    const history = useHistory();
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [siape, setSiape] = useState('');
+  const history = useHistory();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [siape, setSiape] = useState("");
 
-    async function handleRegister(e) {
-        e.preventDefault();
+  async function handleRegister(e) {
+    e.preventDefault();
 
-        const data = {
-            name,
-            email,
-            siape
-        }
-        try {
-            const response = await api.post('professors', data);
-            alert(`Seu ID de acesso: ${response.data.id}`);
-            history.push('/');
-        } catch (err) {
-            alert('Erro, tente novamente.');
-        }
+    const data = {
+      name,
+      email,
+      siape,
+    };
+    if (email.includes("@inf.ufsm.br" || email.includes("@ufsm.br"))) {
+      try {
+        const response = await api.post("professors", data);
+        alert(`Seu ID de acesso: ${response.data.id}`);
+        history.push("/");
+      } catch (err) {
+        alert(
+          "Erro ao conectar com a API, envie um email para cdpaulus@inf.ufsm.br se o problema persistir."
+        );
+      }
+    } else {
+      alert("Por favor, utilize um e-mail institucional");
     }
+  }
 
-    return (
-        <div className="register-container">
-            <div className="content">
-                <section>
-                    <h1>Cadastro</h1>
-                    <p>Faça seu cadastro...</p>
-                    <Link className="back-link" to="/">
-                        <FiArrowLeft size={16} color="#e02041" />
-                        Já tenho Cadastro
-                    </Link>
-                </section>
-
-                <form onSubmit={handleRegister}>
-                    <input
-                        placeholder="Nome professor(a)"
-                        value={name}
-                        onChange={e => setName(e.target.value)}
-                        required
-                    />
-                    <input
-                        type="email"
-                        placeholder="Email"
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
-                        required
-                    />
-                    <input
-                        placeholder="Siape"
-                        value={siape}
-                        onChange={e => setSiape(e.target.value)}
-                        required
-                    />
-
-                    <button className="button" type="submit">Cadastrar</button>
-                </form>
-            </div>
-        </div>
-    );
+  return (
+    <div className="register-container">
+      <div className="content">
+        <section style={{ paddingRight: "2%" }}>
+          <h1>Cadastro</h1>
+          <p>
+            Faça seu cadastro na plataforma e salve seu ID de acesso em um local
+            seguro!
+          </p>
+          <p>Não esqueça de usar um email institucional.</p>
+          <Link className="back-link" to="/">
+            <FiArrowLeft size={16} color="#e02041" />
+            Já tenho Cadastro
+          </Link>
+        </section>
+        <form onSubmit={handleRegister}>
+          <input
+            placeholder="Nome professor(a)"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            placeholder="Siape"
+            value={siape}
+            onChange={(e) => setSiape(e.target.value)}
+            required
+          />
+          <button className="button" type="submit">
+            Cadastrar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
